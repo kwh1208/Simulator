0c924b381f8766c4b89efda81438520acabf91e4
DESKTOP-RF4FDQ0\HP <ceo@dabitsol.com>
2025-02-17 12:57:31 +0900

    [25.02.17] mqtt 추가 gradle

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -10,7 +10,14 @@ module DBPS.main {
     requires static lombok;
     requires org.fxmisc.richtext;
     requires org.slf4j;
+    requires org.eclipse.paho.client.mqttv3;
 
+    // ✅ 명시적으로 패키지 exports
     exports dbps.dbps;
+    exports dbps.dbps.controller;
+    exports dbps.dbps.service;
+    exports dbps.dbps.service.connectManager;
+
+    // ✅ JavaFX FXML에서 Controller 로딩을 허용
     opens dbps.dbps.controller to javafx.fxml;
 }
